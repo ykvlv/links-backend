38eedc42f1a8ce3db7107efa2a3ebed6c3d3d12f
Grigory Yakovlev <71254983+ykvlv@users.noreply.github.com>
2025-05-13 13:43:58 +0300

    links-application init

---
+++ links-application/src/native-test/java/dev/ykvlv/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package dev.ykvlv;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
